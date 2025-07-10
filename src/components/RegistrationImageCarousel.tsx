ac2df3b447eab118e675ab0c0955fac49ebf0ee3
blackjack777x3 <blackjackonelife@gmail.com>
2025-07-10 14:12:39 -0700

    image registration

--- src/components/RegistrationImageCarousel.tsx
+++ src/components/RegistrationImageCarousel.tsx
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 const images = [
   'https://dimesonly.s3.us-east-2.amazonaws.com/realisticvision_ea2691d7-25a7-4cd7-8d4e-cf4826e6c1c3.png',
-  'https://dimesonly.s3.us-east-2.amazonaws.com/realorgasm_1e7aba95-373c-49fd-b90e-b335065488fe.png',
-  'https://dimesonly.s3.us-east-2.amazonaws.com/realorgasm_c2328b2a-bc64-4eab-82ef-a8af1f237d6e.png'
+  'https://dimesonly.s3.us-east-2.amazonaws.com/Kennadie+45.png',
+  'https://dimesonly.s3.us-east-2.amazonaws.com/realisticvision_96184858-4dad-438e-8884-105f6c880251.png'
+  'https://dimesonly.s3.us-east-2.amazonaws.com/eroticgirl_7dd2dfc3-d1ef-4f54-af34-f5ea901d4125-768x1250.png'
+  'https://dimesonly.s3.us-east-2.amazonaws.com/realorgasm_d83e24cd-671a-4515-94fc-0973bd54ece5.png'
+
 ];
 
 const RegistrationImageCarousel: React.FC = () => {
