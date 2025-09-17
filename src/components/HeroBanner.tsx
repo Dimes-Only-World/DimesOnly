a8a46d6dda62968ee49d8527d25585ce67e3003f
blackjack777x3 <blackjackonelife@gmail.com>
2025-09-16 17:53:24 -0700

    Update HeroBanner.tsx
    720

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // Video-only hero: legacy slides removed.
 
 const HeroBanner: React.FC = () => {
-  const phoneSrc = 'https://dimesonlyworld.s3.us-east-2.amazonaws.com/9-16+HOME.mp4';
+  const phoneSrc = 'https://dimesonlyworld.s3.us-east-2.amazonaws.com/9-16+HOME+(2).mp4';
   const desktopSrc = 'https://dimesonlyworld.s3.us-east-2.amazonaws.com/16-9+HOME.mp4';
   return (
     <section className="relative w-full h-screen overflow-hidden bg-black">
