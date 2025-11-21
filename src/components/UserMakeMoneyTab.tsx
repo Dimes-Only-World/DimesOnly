70d4823938250b7dd2f5229526b2f3fcf5e6644e
blackjack777x3 <blackjackonelife@gmail.com>
2025-11-21 10:31:54 -0700

    Revise share message for UserMakeMoneyTab
    Updated share message with new compensation details.

--- src/components/UserMakeMoneyTab.tsx
+++ src/components/UserMakeMoneyTab.tsx
@@ -52,8 +52,10 @@ const UserMakeMoneyTab: React.FC = () => {
   const shareMessage = useMemo(
     () =>
       `Everyone who joins before the app is released will be compensated up to $10,000 to $22,500 a month or more for life just for joining early. There is more money than that to be made on your own. This will be historic.
-If you are a Dime $up to $300,000+ a year
-If you are a male or normal female up to $100,000+ a year
+EXOTICS & STRIPPERS
+Make up to $300,000+ a year
+EVERYONE ELSE
+Make up to $100,000+ a year
 
 You can make unlimited money, the above is just our commitment to thise that join early...
 Any questions? Text or call (707)640-1661 If not, click my link now before positions are gone or the app is released.
